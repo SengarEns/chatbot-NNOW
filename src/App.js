d89a559edc1d583be1303fb78cd84ce954c8110f
SengarEns <pranjal.sengar@ens.enterprises>
2025-04-23 17:35:54 +0530

    fixed more errors

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import ChatbotComponent from './ChatbotComponent'; // Adjust the import path
+import Chatbot from "./chatbot";
+import NnnowChatBot from "./nnnowChatBot";
+import Bot from "./bot";
+import {useSearchParams} from "react-router-dom"
+
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<ChatbotComponent />} />
+        <Route path="/nnnow" element={<NnnowChatBot />} />
+        <Route path="/chat" element={<Chatbot />} />
+        <Route path="/bot" element={<Bot />} />
         {/* Add other routes as needed */}
       </Routes>
     </BrowserRouter>
@@ -14,3 +22,11 @@ function App() {
 }
 
 export default App;
+
+
+
+
+// <NnnowChatBot />
+
+//  <Chatbot />
+// <Bot />
